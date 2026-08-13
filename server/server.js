6910d005ff67df0b8f3295577f0e917c75bfeec0
theMadhavR <madhavr003z@gmail.com>
2026-08-13 14:01:02 +0530

    commit to redeploy

--- server/server.js
+++ server/server.js
@@ -317,6 +317,10 @@ app.post('/api/storyboards', authenticateToken, (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (!process.env.VERCEL) {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+export default app;
